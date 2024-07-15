ad38327ce2c1c94e44d1ce4662e38275ae968566
miqdad242 <miqali242@gmail.com>
2024-07-15 13:17:07 +0530

    tt

--- backend/server.js
+++ backend/server.js
@@ -1,68 +1,59 @@
-const dotenv = require("dotenv").config();
+require("dotenv").config();
 const express = require("express");
-const mongoose= require("mongoose");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const userRoute = require("./routes/userRoute")
-const productRoute = require("./routes/productRoute")
+const userRoute = require("./routes/userRoute");
+const productRoute = require("./routes/productRoute");
 const contactRoute = require("./routes/contactRoute");
-const errorHandler = require("./middleWare/errorMiddleware")
-const cookieParser = require("cookie-parser")
+const errorHandler = require("./middleWare/errorMiddleware");
+const cookieParser = require("cookie-parser");
 const path = require("path");
 // const cloudinary = require("cloudinary").v2;
 
-
-const app= express()
+const app = express();
 const PORT = process.env.PORT || 5000;
-
 
 // cloudinary.config({
 //     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
 //     api_key: process.env.CLOUDINARY_API_KEY,
 //     api_secret: process.env.CLOUDINARY_API_SECRET,
-//   });
+// });
 
-//Middlewares
+// Middlewares
+app.use(cors({
+    origin: ["http://localhost:3000", "https://pinvent-app-frontends.vercel.app", "https://rococo-madeleine-1bedfb.netlify.app"],
+    credentials: true,
+    optionsSuccessStatus: 200,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-// app.use(cors({
-//     origin: ["http://localhost:3000", "https://pinvent-app-frontends.vercel.app",],
-//     credentials: true,
-//     optionsSuccessStatus: 200
-// }));
-app.use(cors({
-    origin: ["https://rococo-madeleine-1bedfb.netlify.app/",],
-    credentials: true,
-    optionsSuccessStatus: 200
-}));
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-//Route Middleware
+// Route Middleware
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/contactus", contactRoute);
 
-
-
-//Routes
-app.get("/", (req,res) =>{
+// Routes
+app.get("/", (req, res) => {
     res.send("Home Page");
-})
+});
 
-
-//Error MiddlewARE
-
+// Error Middleware
 app.use(errorHandler);
 
-//Connect to DB and start server
+// Connect to DB and start server
 mongoose
-            .connect(process.env.MONGO_URI)
-            .then(() => {
-                app.listen(PORT, () => {
-                    console.log(`Server Running on Port ${PORT}`)
-                })
-            })
-            .catch((err)=>console.log(err))
+    .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server Running on Port ${PORT}`);
+        });
+    })
+    .catch((err) => console.log(err));
